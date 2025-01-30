88fbf3203cfbe52d68314619511ce560ad5ac436
sjianzhuo <sjianzhuo@fortinet.com>
2025-01-29 17:04:33 -0800

    add version on pom.xml

--- src/main/java/com/example/Utils.java
+++ src/main/java/com/example/Utils.java
@@ -3,7 +3,11 @@ package com.example;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.Random;
 
 public class Utils {
 
@@ -30,6 +34,22 @@ public class Utils {
         // DES key length is 56 bits
         keyGen.init(56);
         return keyGen.generateKey();
+    }
+
+    public static String generatePRNG() {
+        Random random = new Random(System.currentTimeMillis());
+        int accountID = random.nextInt();
+        return String.valueOf(accountID);
+    }
+
+    public static String hashMD5(String data) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] hashBytes = md.digest(data.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashBytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
