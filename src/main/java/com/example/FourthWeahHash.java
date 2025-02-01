5956f0fc3135719b7b498f16c587946cf8d1d9f5
sjianzhuo <sjianzhuo@fortinet.com>
2025-01-31 16:58:54 -0800

    add 4th code for test

---
+++ src/main/java/com/example/FourthWeahHash.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class FourthWeahHash {
+    public static String hashMD5(String data) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] hashBytes = md.digest(data.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashBytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
