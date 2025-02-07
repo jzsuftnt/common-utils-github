d08d75a03bdefaf841f7ee9e147097b0fb8eee35
sjianzhuo <sjianzhuo@fortinet.com>
2025-02-07 14:06:27 -0800

    add new folder and code

---
+++ src/main/java/com/newexample/WeakHash.java
@@ -0,0 +1,29 @@
+package com.newexample;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class WeakHash {
+    public static String hashMD5(String data) throws NoSuchAlgorithmException {
+        MessageDigest md1 = MessageDigest.getInstance("MD5");
+        MessageDigest md2 = MessageDigest.getInstance("MD5");
+        System.out.println("MessageDigest md1 = MessageDigest.getInstance(\"MD5\")");
+        byte[] hashBytes = md1.digest(data.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashBytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+    public static String hashMD51(String data) throws NoSuchAlgorithmException {
+        MessageDigest md1 = MessageDigest.getInstance("MD5");
+        System.out.println("MessageDigest md1 = MessageDigest.getInstance(\"MD5\")");
+        byte[] hashBytes = md1.digest(data.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashBytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+}
