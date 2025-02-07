6a02ccbb567d0ac2b4545b273afb5cdbde94c41a
sjianzhuo <sjianzhuo@fortinet.com>
2025-02-07 15:27:16 -0800

    del comments codes

--- src/main/java/com/newexample/WeakHash.java
+++ src/main/java/com/newexample/WeakHash.java
@@ -6,7 +6,6 @@ import java.security.NoSuchAlgorithmException;
 public class WeakHash {
     public static String hashMD5(String data) throws NoSuchAlgorithmException {
         MessageDigest md1 = MessageDigest.getInstance("MD5");
-        // MessageDigest md2 = MessageDigest.getInstance("MD5");
         System.out.println("MessageDigest md1 = MessageDigest.getInstance(\"MD5\")");
         byte[] hashBytes = md1.digest(data.getBytes());
         StringBuilder sb = new StringBuilder();
@@ -16,14 +15,4 @@ public class WeakHash {
         return sb.toString();
     }
 
-    // public static String hashMD51(String data) throws NoSuchAlgorithmException {
-    //     MessageDigest md1 = MessageDigest.getInstance("MD5");
-    //     System.out.println("MessageDigest md1 = MessageDigest.getInstance(\"MD5\")");
-    //     byte[] hashBytes = md1.digest(data.getBytes());
-    //     StringBuilder sb = new StringBuilder();
-    //     for (byte b : hashBytes) {
-    //         sb.append(String.format("%02x", b));
-    //     }
-    //     return sb.toString();
-    // }
 }
